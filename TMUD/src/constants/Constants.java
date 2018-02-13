75a784b2bf71499339d5a3afa5c77ef26b6b40cd
Tim Van Hoose <trvanhoose@gmail.com>
2018-02-13 17:13:15 -0600

    Initial commit

---
+++ TMUD/src/constants/Constants.java
@@ -0,0 +1,23 @@
+package constants;
+
+
+
+public class Constants {
+	final char PLAYER = '@';
+	final char OPEN_TILE = '.';
+	final char SPECIAL_ITEM = '$';
+	final char MOB = '*';
+	final char TOP_BOTTOM_DOOR = '-';
+	final char LEFT_RIGHT_DOOR = '|';
+	final char DARKENED_TILE = '#';
+	final char STAIRWELL = '_';
+	final int TEST_ROOM_WIDTH = 5;
+	final int TEST_ROOM_LENGTH = 5;
+	final int TEST_HALL_LENGTH = 10;
+	final int TEST_HALL_WIDTH = 1;
+	
+	/*
+	 * TODO: add color constants (hex)
+	 */
+
+}
